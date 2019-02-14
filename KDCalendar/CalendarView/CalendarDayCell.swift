4d4868e33ce386f1c45aa76834db05fb7d185cc8
rithlkh2 <rithronlkh@gmail.com>
2019-02-14 16:03:02 +0700

    add sample calendar

--- KDCalendar/CalendarView/CalendarDayCell.swift
+++ KDCalendar/CalendarView/CalendarDayCell.swift
@@ -26,6 +26,9 @@
 import UIKit
 
 open class CalendarDayCell: UICollectionViewCell {
+    let textLabel   = UILabel()
+    let dotsView    = UIView()
+    let bgView      = UIView()
     
     override open var description: String {
         let dayString = self.textLabel.text ?? " "
@@ -46,9 +49,15 @@ open class CalendarDayCell: UICollectionViewCell {
             case true:
                 self.bgView.backgroundColor = CalendarView.Style.cellColorToday
                 self.textLabel.textColor    = CalendarView.Style.cellTextColorToday
+//                self.textLabel.text    = "Test"
             case false:
-                self.bgView.backgroundColor = CalendarView.Style.cellColorDefault
+//                self.bgView.backgroundColor = CalendarView.Style.cellColorDefault
+                self.bgView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
                 self.textLabel.textColor = CalendarView.Style.cellTextColorDefault
+                
+                self.textLabel.text    = (self.textLabel.text ?? "") + ""
+//                self.textMoonLabel.text    = (self.textMoonLabel.text ?? "") + "(s)"
+//                self.textMoonLabel.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
             }
         }
     }
@@ -61,6 +70,8 @@ open class CalendarDayCell: UICollectionViewCell {
                 self.textLabel.textColor = CalendarView.Style.cellTextColorWeekend
             case false:
                 self.textLabel.textColor = CalendarView.Style.cellTextColorDefault
+                self.textMoonLabel.text    = (self.textMoonLabel.text ?? "") + "(s)"
+//                self.textMoonLabel.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
             }
         }
     }
@@ -81,14 +92,11 @@ open class CalendarDayCell: UICollectionViewCell {
     
     
     
-    let textLabel   = UILabel()
-    let dotsView    = UIView()
-    let bgView      = UIView()
+    let textMoonLabel = UILabel()
     
     override init(frame: CGRect) {
-        
         self.textLabel.textAlignment = NSTextAlignment.center
-        
+        self.textMoonLabel.textAlignment = NSTextAlignment.left
         
         self.dotsView.backgroundColor = CalendarView.Style.cellEventColor
         
@@ -98,7 +106,7 @@ open class CalendarDayCell: UICollectionViewCell {
         self.addSubview(self.textLabel)
         
         self.addSubview(self.dotsView)
-        
+        self.addSubview(self.textMoonLabel)
     }
     
     
@@ -122,11 +130,14 @@ open class CalendarDayCell: UICollectionViewCell {
         
         self.bgView.frame           = elementsFrame
         self.textLabel.frame        = elementsFrame
+        self.textMoonLabel.frame        = elementsFrame
         
         let size                            = self.bounds.height * 0.08 // always a percentage of the whole cell
         self.dotsView.frame                 = CGRect(x: 0, y: 0, width: size, height: size)
         self.dotsView.center                = CGPoint(x: self.textLabel.center.x, y: self.bounds.height - (2.5 * size))
         self.dotsView.layer.cornerRadius    = size * 0.5 // round it
+        
+//        self.dotsView.center                = CGPoint(x: self.textMoonLabel.center.x, y: self.bounds.height - (2.5 * size))
         
         switch CalendarView.Style.cellShape {
         case .square:
